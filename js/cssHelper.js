d31384a1b16deb6d82d12b58254664ae9f429c82
Aruiik <Goodfd59@gmail.com>
2025-04-28 21:41:24 +0200

    css update

--- js/cssHelper.js
+++ js/cssHelper.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
         el.style.animationDelay = (index * 0.1) + 's';
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.previews').forEach((el, index) => {
+        el.style.animationDelay = (index * 0.1) + 's';
+    });
+});
